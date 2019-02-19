4f26fafe6092b4476b332eb886c35c3e543d8b3c
Jamie <itsdeadjim@hotmail.co.uk>
2019-02-19 17:51:37 +0000

    Fixed Collision Issue
    Using Tags was able to identify which object hit which items and preventing the floor from destroying the sign

--- Assets/Scripts/DestructibleController.cs
+++ Assets/Scripts/DestructibleController.cs
@@ -5,21 +5,13 @@ using UnityEngine;
 public class DestructibleController : MonoBehaviour
 {
     public GameObject destroyedVersion;
-    public GameObject sign;
-    private Vector3 pos;
-    private Quaternion rot;
 
     void OnTriggerEnter(Collider other)
     {
-        Instantiate(destroyedVersion, transform.position, transform.rotation);
-        pos = new Vector3(5, 2, -8);
-        rot = transform.rotation;
-        //Instantiate(sign, pos, rot);
-        Destroy(gameObject);
-    }
-
-    void OnDestroy()
-    {
-        //Instantiate(sign, pos, rot);
+        if (other.tag == "Player")
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
