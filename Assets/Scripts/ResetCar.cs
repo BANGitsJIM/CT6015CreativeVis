b5b318200eecfc8a58f4e236d94375807279fd55
Jamie <itsdeadjim@hotmail.co.uk>
2019-02-19 14:37:59 +0000

    Added fall script
    Car resets to its original position when falling

---
+++ Assets/Scripts/ResetCar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCar : MonoBehaviour
+{
+    public float threshold;
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold)
+        {
+            transform.position = new Vector3(5, 2, -8);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
