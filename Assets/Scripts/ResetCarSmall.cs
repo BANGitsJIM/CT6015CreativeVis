9df42c6cb0981a1f3b73ab8cb2434ed5224b3797
Jamie Laurence <itsdeadjim@hotmail.co.uk>
2019-02-22 17:12:43 +0000

    Updated the Small City Scene and Scripts
    Updated the smaller version of the city and updated the scripts for the small city.

---
+++ Assets/Scripts/ResetCarSmall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCarSmall : MonoBehaviour
+{
+    public float threshold;
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold)
+        {
+            transform.position = new Vector3(6, 0, -8);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/SmallSignHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallSignHandler : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject terrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Spawn two objects when initialising start.
+        generateObjectOnTerrain();
+    }
+
+    public void generateObjectOnTerrain()
+    {
+        //Generate random x,z,y position on the terrain
+        float randX = UnityEngine.Random.Range(-6, 15);
+        float randZ = UnityEngine.Random.Range(6, -15);
+        float randRot = UnityEngine.Random.Range(-360, 360);
+        float yVal = 0.2f;
+
+        //Generate the Prefab on the generated position
+        GameObject objInstance = (GameObject)Instantiate(prefab, new Vector3(randX, yVal, randZ), Quaternion.Euler(-90, randRot, 90));
+
+        //Debug.Log("Spawn at X:" + randX + "  Z:" + randZ);
+    }
+}
