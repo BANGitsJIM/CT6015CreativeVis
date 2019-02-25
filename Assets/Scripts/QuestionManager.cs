c778eede18386188b1b476c81dc47b5a289996e1
Jamie Laurence <itsdeadjim@hotmail.co.uk>
2019-02-25 18:46:50 +0000

    Added question manager
    Added question manager and quiz object.
    also added BebasNeue.

---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class QuestionManager: MonoBehaviour
+{
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    void Start ()
+    {
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+
+        GetRandomQuestion();
+        Debug.Log(currentQuestion.fact + " is " + currentQuestion.isTrue);
+    }
+
+    void GetRandomQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        //Remove the question from the array.
+        unansweredQuestions.RemoveAt(randomQuestionIndex);
+
+    }
+}
