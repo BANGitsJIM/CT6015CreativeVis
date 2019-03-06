09e432984b1c5127fdd8a299198ddff0b5ee293c
Jamie <itsdeadjim@hotmail.co.uk>
2019-03-06 18:09:59 +0000

    Animated quiz
    added smooth intro and exit animations for the quiz

---
+++ Assets/Scripts/FactFader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FactFader : MonoBehaviour
+{
+    public Animator animator;
+
+    public void FadeOutLevel()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+}
